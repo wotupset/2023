a70d7629e70a31de6ce0d55d563e4b612a4ea160
断固非難 <4385327+wotupset@users.noreply.github.com>
2023-02-05 20:28:08 +0800

    Add files via upload

---
+++ 02/05/del.bat
@@ -0,0 +1,24 @@
+pause
+
+del /q image
+copy list.bat .\image\_.bat
+del /q video
+copy list.bat .\video\_.bat
+
+echo 123 >twitter.txt
+
+exit
+pause
+exit
+
+安靜刪除資料夾中的全部檔案 資料夾留著
+del /q video
+copy list.bat .\video\list.bat
+
+echo 123 >-.txt
+
+
+copy desktop.ini .\list\desktop.ini
+del /q list2021
+copy list.bat .\list2021\list.bat
+copy list.bat .\list2021\list.bat
